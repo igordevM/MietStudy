554f7fa86f733dba5628d7520c34a4b939f8038c
igor <igordevm@gmail.com>
2020-06-04 11:42:48 +0300

    Done

--- lab4/src/parallel_min_max/parallel_min_max.c
+++ lab4/src/parallel_min_max/parallel_min_max.c
@@ -114,7 +114,7 @@ int main(int argc, char **argv) {
     }
 
     if (seed == -1 || array_size == -1 || pnum == -1) {
-        printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\ --timeout \"num\" \n",
+        printf("Usage: %s --seed \"num\" --array_size \"num\" --pnum \"num\" --timeout \"num\" \n",
                argv[0]);
         return 1;
     }
